2dac4b26c695c910cb42a8317dd35a154f15e5a3
Alan Jones <aljones1816@gmail.com>
2020-05-19 10:06:59 -0400

    added basic css styling for buttons grid and basic html structure

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+function add (...args) {
+	return args.reduce(function (acc, cur) {
+		return acc + cur;
+	})
+}
+
+function subtract (num1,num2) {
+	if (typeof(num1) === 'number' && typeof(num2) === 'number') {
+		return(num1-num2);
+		} else return;
+}
+
+function sum () {
+	
+}
+
+function multiply () {
+	
+}
+
+function power() {
+	
+}
+
+function factorial() {
+	
+}
+
