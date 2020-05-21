f2f29a8bd3a413159c208fff52ed5074885695d4
Alan Jones <aljones1816@gmail.com>
2020-05-21 10:53:29 -0400

    made the calculator calculate!

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 let string = '';
 let buttons = document.querySelectorAll('.visi-but');
+let ans = '';
 
 // basic arithmetic functions - each takes user input and returns a single number
 function add(numArray) {
@@ -10,7 +11,7 @@ function add(numArray) {
 }
 
 function subtract(numArray) {
-	numArray = numArray.map(number);
+	numArray = numArray.map(Number);
 	return numArray.reduce(function (acc, cur) {
 		return acc - cur;
 	})
@@ -18,14 +19,14 @@ function subtract(numArray) {
 
 
 function multiply(numArray) {
-	numArray = numArray.map(number);
+	numArray = numArray.map(Number);
 	return numArray.reduce(function (acc, cur) {
 		return acc * cur;
 	})
 }
 
 function divide(numArray) {
-	numArray = numArray.map(number)
+	numArray = numArray.map(Number);
 	return numArray.reduce(function (acc, cur) {
 		return acc / cur;
 	})
@@ -33,22 +34,57 @@ function divide(numArray) {
 
 // function that takes an array of numbers and calls one of the arithmetic functions
 function operate(operator, numArray) {
-	if (operator == 'add') {
+	if (operator == '+') {
 		return add(numArray);
-	} else if (operator == 'subtract') {
-		return substract(numArray);
-	} else if (operator == 'multiply') {
+	} else if (operator == '-') {
+		return subtract(numArray);
+	} else if (operator == '*') {
 		return multiply(numArray);
-	} else if (operator == 'divide') {
+	} else if (operator == '/') {
 		return divide(numArray);
 	} else return;
+}
+
+
+// function that breaks up a string into an array on spaces 
+function breakUp(string) {
+	return string.split(" ");
+}
+
+// function to loop through an array of strings and find and evaluate expressions following pemdas
+function evalToo(string) {
+	strArray = breakUp(string);
+
+	while (strArray.indexOf('*') > -1) {
+		strArray.splice(strArray.indexOf('*') - 1, 3,
+			operate('*', [strArray[strArray.indexOf('*') - 1], strArray[strArray.indexOf('*') + 1]]))
+	}
+	while (strArray.indexOf('/') > -1) {
+		strArray.splice(strArray.indexOf('/') - 1, 3,
+			operate('/', [strArray[strArray.indexOf('/') - 1], strArray[strArray.indexOf('/') + 1]]))
+	}
+	while (strArray.indexOf('+') > -1) {
+		strArray.splice(strArray.indexOf('+') - 1, 3,
+			operate('+', [strArray[strArray.indexOf('+') - 1], strArray[strArray.indexOf('+') + 1]]))
+	}
+	while (strArray.indexOf('-') > -1) {
+		strArray.splice(strArray.indexOf('-') - 1, 3,
+			operate('-', [strArray[strArray.indexOf('-') - 1], strArray[strArray.indexOf('-') + 1]]))
+	}
+
+	return strArray[0];
 }
 
 // function for deleting last entry
 function delLast() {
 	if (string.length > 1) {
-		string = string.substring(0, string.length - 1);
-		updateDisplay();
+		if (string[string.length - 1] == ' ') {
+			string = string.substring(0, string.length - 3);
+			updateDisplay();
+		} else {
+			string = string.substring(0, string.length - 1);
+			updateDisplay();
+		}
 	} else {
 		string = '0';
 		updateDisplay();
@@ -66,7 +102,11 @@ function updateHistory() {
 	if (string.length == 0) {
 		string = '0';
 	}
-	document.getElementById('history').innerHTML = string + ' ='
+	document.getElementById('history').innerHTML = string + ' =';
+	document.getElementById('current').innerHTML = evalToo(string);
+	ans = evalToo(string);
+	string = evalToo(string);
+
 }
 
 // function to reset display when clear is pressed
@@ -74,25 +114,11 @@ function resetDisplay() {
 	if (string.length == 0) {
 		string = '0';
 	}
-	document.getElementById('history').innerHTML = 'ans = ' + string;
+	document.getElementById('history').innerHTML = 'ans = ' + ans;
 	string = '0';
 	updateDisplay();
 	string = '';
 
-}
-
-// function to loop through an array of strings and find and evaluate expressions following pemdas
-function evalToo(strArray) {
-while (strArray.indexOf('+') >-1) {
-	strArray.splice(strArray.indexOf('+')-1,3,
-	add([strArray[strArray.indexOf('+')-1],strArray[strArray.indexOf('+')+1]]))
-}
-}
-
-expArray = [];
-// function that breaks up a string into an array on spaces 
-function breakUp (string) {
-	expArray = string.split(" ");
 }
 
 // event listener for retrieving user input and displaying it on screen
